2b0b26389967f3f6b0a66a8cfc56b736c7d7f3f3
Vanoverberghe Stephanie <vanoverberghe.stephanie@outlook.fr>
2022-11-17 16:19:21 +0100

    premier dépot

---
+++ premiere_partie/public/assets/js/variable.js
@@ -0,0 +1,28 @@
+// Exercice 1
+
+let message = 'Affiche dans la console du navigateur';
+
+console.log(message);
+
+// Exercice 2
+
+let firstPart = 'Première partie d\'une phrase ';
+let lastPart = 'dernière partie de la même phrase';
+
+console.log(firstPart + lastPart);
+
+// Exercice 3
+
+let firstname = 'Denis';
+let age = 25;
+let city = 'Amiens';
+
+console.log(`Bonjour, je m\'appelle ${firstname}, j\ai ${age} ans et j\habite ${city}`);
+
+// Exercice 4
+
+let num = 43;
+let txt = '7';
+let sum = num + parseInt('7');
+
+console.log(`La somme de num et de txt est ${sum}`)
