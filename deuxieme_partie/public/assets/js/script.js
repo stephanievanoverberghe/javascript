355a8dc433e8d38b42d1cbbd0f88b719c2c807f1
Vanoverberghe Stephanie <vanoverberghe.stephanie@outlook.fr>
2022-11-24 17:46:28 +0100

    Dépot

---
+++ deuxieme_partie/public/assets/js/script.js
@@ -0,0 +1,29 @@
+// Exercice 1
+
+// Déclaration de variables
+
+let elForm = document.getElementById('form');
+let passOne = document.getElementById('passOne');
+let passTwo = document.getElementById('passTwo');
+
+
+// Déclaration d'évenements
+
+elForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (passOne = passTwo) {
+        
+    } else {
+
+    }
+})
+    console.log(elForm);
+
+// Exercice 2
+
+// Exercice 3
+
+// Exercice 4
+
+// Exercice 5
