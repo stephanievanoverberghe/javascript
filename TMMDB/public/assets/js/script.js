28cc734eca49af102e0bbd318111f1f05f9f8ad9
Vanoverberghe Stephanie <vanoverberghe.stephanie@outlook.fr>
2022-12-03 11:36:42 +0100

    dépot shifumi 2

--- TMMDB/public/assets/js/script.js
+++ TMMDB/public/assets/js/script.js
@@ -6,14 +6,19 @@ fetch("public/assets/js/movies.json")
             let url = movie.poster_path;
             let description = movie.overview;
             let vote = movie.vote_average;
-            card.innerHTML += `<div class="card" style="width: 18rem;">
-            <img src=${url} class="card-img-top" alt="image">
-            <div class="card-body">
-                <h5 class="card-title">${title}</h5>
-                <p class="card-text">${description}</p>
-                <p class="card-vote">${vote}</p>
-            </div>
-        </div>`
+            card.innerHTML +=
+                `<div class="col-12 col-md-6 col-lg-3" >
+                    <div style="" class="m-3">
+                    <div class="card ">
+                        <img src=${url} class="card-img-top" alt="image">
+                        <div class="card-body">
+                            <h5 class="card-title">${title}</h5>
+                            <p class="card-text overflow-ellipsis">${description}</p>
+                            <p class="card-vote">${vote}</p>
+                        </div>
+                    </div>
+                    </div>
+                </div>`
         })
     })
 
